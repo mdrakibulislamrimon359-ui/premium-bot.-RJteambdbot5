f4fe8c4f1f429608f36a8fbd5e65eae76b88aa68
mdrakibulislamrimon359-ui <mdrakibulislamrimon359@gmail.com>
2026-09-24 11:35:29 +0600

    Delete bot.py

--- bot.py
+++
@@ -1,7 +0,0 @@
-# Main Telegram bot
-import os
-from telegram.ext import Application
-BOT_TOKEN=os.getenv("BOT_TOKEN")
-app=Application.builder().token(BOT_TOKEN).build()
-print("RJ OTP Universe Bot")
-app.run_polling()
